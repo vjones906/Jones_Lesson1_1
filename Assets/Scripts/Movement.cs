02cb0270dba1da09c35f3200518ade0af3b5a334
Vanessa <vanessamaikojones@gmail.com>
2020-02-10 10:48:38 -0800

    added vehicle movements

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float verSpeed = 20;
+    public float hozSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // Here is my comment
+        transform.Translate(Vector3.forward * Time.deltaTime * verSpeed * verticalInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * hozSpeed * horizontalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * hozSpeed * horizontalInput);
+    }
+}
